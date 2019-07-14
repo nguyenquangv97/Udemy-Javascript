874ac733727ead3f5d653df588dc1c21653398af
Quang Nguyen <nguyenquangv97@gmail.com>
2019-07-14 12:56:07 -0700

    initial commit

---
+++ Section 2 Javascript Language Basics/2-JS-basics/script.js
@@ -0,0 +1,118 @@
+/**************************
+* Variables and data types
+*/
+/*
+var firstName = 'Quang';
+console.log(firstName);
+
+var lastName = 'Nguyen';
+var age = 21;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'student';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
+*/
+
+
+/*************************************
+* Variable mutation and type coercion 
+*/
+
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver'
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(lastName + ' ' + firstName);
+
+*/
+
+
+
+/*********************************
+* Basic operators
+*/
+var year, yearJohn, yearMark;
+now = 2018
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+
+// Logical operators
+var JohnOlder = ageJohn > ageMark;
+console.log(JohnOlder);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
