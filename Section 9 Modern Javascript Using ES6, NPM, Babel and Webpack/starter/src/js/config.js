ac5cf401ed80ca8e527a54ff19f10b6f7ea5fa54
nguyenquangv97 <51775587+nguyenquangv97@users.noreply.github.com>
2019-09-03 18:59:48 -0700

    Finish Forkify

---
+++ Section 9 Modern Javascript Using ES6, NPM, Babel and Webpack/starter/src/js/config.js
@@ -0,0 +1,7 @@
+const cors_api_host = 'cors-anywhere.herokuapp.com';
+export const proxy = `https://${cors_api_host}/`;
+export const key = 'f74bd8846e434d07eb145b6bfba5b7e1';
+// 173ea309c4040cb624891e7d3df89675
+// 04fea95c5243441929ca4527ac5b7765
+// f74bd8846e434d07eb145b6bfba5b7e1
+// a8806aa24976ee49e1117a0979c4c0a9
--- Section 9 Modern Javascript Using ES6, NPM, Babel and Webpack/starter/src/js/models/Search.js
+++ Section 9 Modern Javascript Using ES6, NPM, Babel and Webpack/starter/src/js/models/Search.js
@@ -1,1 +1,17 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+import { key, proxy } from '../config';
+
+export default class Search {
+	constructor(query) {
+		this.query = query;
+	}
+
+	async getResult() {
+		try {
+			const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+			this.result = res.data.recipes;
+		} catch (error) {
+			alert(error);
+		}
+	}
+}
